8c1d35f04ecaee43e3ed08542d90f1652450a9e4
akocakaya <abdullahkocakaya@hotmail.com>
2019-09-28 22:15:06 +0300

    jsonwebtoken added. login mutation created

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,13 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { User } from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -12,6 +19,35 @@ export class UserResolver {
     @Query( () => [User] )
     users() {
         return User.find();
+    }
+
+    @Mutation(() => LoginResponse)
+    async login (
+        @Arg('email') email: string,
+        @Arg('password') password: string
+        ): Promise<LoginResponse> {
+
+        const user = await User.findOne({ where: { email }});
+
+        if(!user) {
+            throw new Error ('no user');
+        }
+
+        const valid = await compare(password, user.password);
+
+        if(!valid) {
+            throw new Error("wrong password");
+        }
+
+        //login successfull
+
+        return {
+            accessToken : sign(
+                { userId: user.id }, 
+                'secret', 
+                { expiresIn: '15m' }
+            )
+        };
     }
 
     @Mutation(() => Boolean)
