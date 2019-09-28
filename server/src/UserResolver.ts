0e26f74ff0ebcccc82acced1df26a8f0c97277bd
akocakaya <abdullahkocakaya@hotmail.com>
2019-09-29 01:44:45 +0300

    cookie-parser added to put cookie to req. refresh token logic created.

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -4,6 +4,7 @@ import { User } from './entity/User';
 import { MyContext } from './MyContext';
 import { createRefreshToken, createAccessToken } from './auth';
 import { isAuth } from './isAuth';
+import { sendRefreshToken } from './sendRefreshToken';
 
 @ObjectType()
 class LoginResponse {
@@ -51,11 +52,7 @@ export class UserResolver {
 
         //login successfull
 
-        res.cookie(
-            'jid', 
-            createRefreshToken(user),
-            { httpOnly: true }
-        );
+        sendRefreshToken(res, createRefreshToken(user));
 
         return {
             accessToken: createAccessToken(user)
