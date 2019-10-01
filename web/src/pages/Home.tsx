6f5aaae9a36966c760d6887192a6a6a0181961cc
akocakaya <abdullahkocakaya@hotmail.com>
2019-10-01 23:29:18 +0300

    get all users query added to home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useUsersQuery } from '../generated/graphql';
 
 interface Props {
 
 }
 
 export const Home: React.FC<Props> = () => {
+
+    const { data, } = useUsersQuery({ fetchPolicy: 'network-only' });
+
+    if (!data) {
+        return <div>loading...</div>
+    }
+
     return (
         <div>
-            home page
+            <div>users:</div>
+            <ul>
+                {data.users.map(u => {
+                    return (
+                        <li key={u.id}>
+                            {u.email}, {u.id}
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
