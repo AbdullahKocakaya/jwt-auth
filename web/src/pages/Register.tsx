504ceeb64aae98217915ef7071f77f8ac41e5f2f
akocakaya <abdullahkocakaya@hotmail.com>
2019-10-01 23:11:54 +0300

    register page form elements created

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 interface Props {
 
 }
 
 export const Register: React.FC<Props> = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>register page</div>
+        <form onSubmit={e => {
+            e.preventDefault();
+            console.log('form submitted');
+            console.log(email, password);
+        }}>
+            <div>
+                <input 
+                    value       = { email } 
+                    placeholder = "email" 
+                    onChange    = { e => {
+                        setEmail(e.target.value);
+                    }} 
+                />
+            </div>
+            <div>
+                <input 
+                    type        = 'password'
+                    value       = { password } 
+                    placeholder = "password" 
+                    onChange    = { e => {
+                        setPassword(e.target.value);
+                    }} 
+                />
+            </div>
+            <button type="submit">register</button>
+        </form>
     );
 }
