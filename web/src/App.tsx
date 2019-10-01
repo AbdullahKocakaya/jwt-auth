0eea5d2e0f96764a8cb7192b2d75f1a774f42882
akocakaya <abdullahkocakaya@hotmail.com>
2019-10-01 22:33:06 +0300

    first query to our server with apollo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 const App: React.FC = () => {
-  return (
-    <div>hello world</div>
-  );
+    const { data, loading } = useQuery(gql`
+        {
+            hello
+        }
+    `);
+
+    if(loading) {
+        return <div>loading...</div>
+    }
+
+    return (
+        <div>
+            { JSON.stringify(data) }
+        </div>
+    );
 }
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const serverPort = 4000;
+
+const client = new ApolloClient({
+    uri: `http://localhost:${serverPort}/graphql`
+})
+
+ReactDOM.render(
+    <ApolloProvider client= { client }> 
+        <App /> 
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
