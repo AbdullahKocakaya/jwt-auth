95728f311ce03e27ab3a9435b7801d6666db7073
akocakaya <abdullahkocakaya@hotmail.com>
2019-10-01 23:33:17 +0300

    login with mutation

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useLoginMutation } from '../generated/graphql';
+import { RouteComponentProps } from 'react-router';
 
-interface Props {
+export const Login: React.FC<RouteComponentProps> = ({history}) => {
 
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-export const Login: React.FC<Props> = () => {
+    const [login] = useLoginMutation();
+
     return (
-        <div>Login page</div>
+        <form onSubmit={async e => {
+            e.preventDefault();
+            console.log('form submitted');
+            const response = await login({
+                variables: {
+                    email,
+                    password
+                }
+            });
+
+            console.log(response)
+            
+            history.push("/");
+        }}>
+            <div>
+                <input 
+                    value       = { email } 
+                    placeholder = "email" 
+                    onChange    = { e => {
+                        setEmail(e.target.value);
+                    }} 
+                />
+            </div>
+            <div>
+                <input 
+                    type        = 'password'
+                    value       = { password } 
+                    placeholder = "password" 
+                    onChange    = { e => {
+                        setPassword(e.target.value);
+                    }} 
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>
     );
 }
