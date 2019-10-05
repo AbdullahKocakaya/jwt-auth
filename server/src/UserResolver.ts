1047b9813b0040c8fdeef2e6a6f10506c64a7ec9
akocakaya <abdullahkocakaya@hotmail.com>
2019-10-05 14:29:51 +0300

    logout feature added

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -69,6 +69,14 @@ export class UserResolver {
         return true;
     }
 
+    @Mutation(() => Boolean)
+    async logout(@Ctx() { res }: MyContext) {
+        
+        sendRefreshToken(res, '');
+
+        return true;
+    }
+
     @Mutation(() => LoginResponse)
     async login (
         @Arg('email') email: string,
