69897dd7a460d46f227c26b7bf9fc9952f522b18
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2021-01-04 23:13:11 +0300

    readme added and catch added to server connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,80 +15,65 @@ import { sendRefreshToken } from './sendRefreshToken';
 const port = 4000;
 
 (async () => {
-    const app = express();
+    try {
+        const app = express();
 
-    app.use(
-        cors({
-            origin: "http://localhost:3000",
-            credentials: true
+        app.use(
+            cors({
+                origin: "http://localhost:3000",
+                credentials: true
+            })
+        );
+
+        app.use(cookieParser());
+
+        app.get('/', (_req, res) => res.send('hello'));
+
+        app.post("/refresh_token",async (req, res) => {
+            const token = req.cookies.jid;
+
+            if (!token) {
+                return res.send({ ok: false, accessToken: ""});
+            }
+
+            let payload: any = null;
+            try {
+                payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+            } catch(err) {
+                console.log(err);
+                return res.send({ ok: false, accessToken: ""});
+            }
+
+            const user = await User.findOne({ id: payload.userId});
+
+            if (!user) {
+                return res.send({ ok: false, accessToken: ""});
+            }
+
+            if (user.tokenVersion !== payload.tokenVersion) {
+                return res.send({ ok: false, accessToken: ""});
+            }
+
+            sendRefreshToken(res, createRefreshToken(user));
+
+            return res.send({ ok: false, accessToken: createAccessToken(user) });
         })
-    );
 
-    app.use(cookieParser());
+        await createConnection();
 
-    app.get('/', (_req, res) => res.send('hello'));
+        const apolloServer = new ApolloServer({
+            schema: await buildSchema({
+                resolvers: [UserResolver]
+            }),
+            context: ( ({ req, res }) => ({ req, res }) )
+        });
 
-    app.post("/refresh_token",async (req, res) => {
-        const token = req.cookies.jid;
+        apolloServer.applyMiddleware({ app, cors: false });
 
-        if (!token) {
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        let payload: any = null;
-        try {
-            payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
-        } catch(err) {
-            console.log(err);
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        const user = await User.findOne({ id: payload.userId});
-
-        if (!user) {
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        if (user.tokenVersion !== payload.tokenVersion) {
-            return res.send({ ok: false, accessToken: ""});
-        }
-
-        sendRefreshToken(res, createRefreshToken(user));
-
-        return res.send({ ok: false, accessToken: createAccessToken(user) });
-    })
-
-    await createConnection();
-
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [UserResolver]
-        }),
-        context: ( ({ req, res }) => ({ req, res }) )
-    });
-
-    apolloServer.applyMiddleware({ app, cors: false });
-
-    app.listen(port, () => {
-        console.log("express server started port on : " + port);
-    });
-
+        app.listen(port, () => {
+            console.log("express server started port on : " + port);
+        });
+    } catch(err) {
+        console.log(err);
+    }
 }) ();
-
-// createConnection().then(async connection => {
-
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-
-// }).catch(error => console.log(error));
